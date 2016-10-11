ff7cdf87bd6e2642209d180e8cfa7bac59e1b7eb
neo30904 <ajay.30904@gmail.com>
2016-10-12 02:14:00 +0530

    Reading Tmx File and adding to the view also creating player class

--- Classes/game/GameView.cpp
+++ Classes/game/GameView.cpp
@@ -8,6 +8,7 @@
 
 #include "GameView.hpp"
 #include "GameCommon.h"
+#include "Player.hpp"
 
 
 cocos2d::Layer* GameView::CreateLayer()
@@ -27,6 +28,10 @@ bool GameView::init()
     level_map_ = nullptr;
     map_objects_ = nullptr;
     
+    
+    viewport_ = cocos2d::Vec2::ZERO;
+    viewport_offset_ = cocos2d::Vec2::ZERO;
+    
     AddLevel();
     
     GAMELOG("Setting the View for the Game");
@@ -41,15 +46,8 @@ void GameView::AddLevel()
     {
         addChild(level_map_);
         
-        
         map_objects_ = level_map_->getObjectGroup("Objects");
-        
-        cocos2d::ValueMap spawn_points = map_objects_->getObject("SpawnPoint");
-        cocos2d::Vec2 player_position(spawn_points["x"].asInt() , spawn_points["y"].asInt());
-        
-        player_sprite_ = cocos2d::Sprite::create(cocos2d::FileUtils::getInstance()->fullPathForFilename("tiled/Player.png"));
-        player_sprite_->setPosition(cocos2d::Vec2(player_position.x , player_position.y));
-        addChild(player_sprite_);
+        CreatePlayer();
         
     }
     
@@ -58,18 +56,39 @@ void GameView::AddLevel()
         GAMELOG("Oops problem reading the Map File");
     }
     
+    UpdateView();
     
+}
+
+
+void GameView::CreatePlayer()
+{
+    
+    GAMELOG("Creating Player");
+    
+    player_ = new Player();
+    player_->set_type(kTypePlayer);
+    
+    cocos2d::ValueMap spawn_points = map_objects_->getObject("SpawnPoint");
+    cocos2d::Vec2 player_position(spawn_points["x"].asInt() , spawn_points["y"].asInt());
+    
+    player_->CreateSprite(player_position_);
+}
+
+void GameView::UpdateView()
+{
     cocos2d::Size winSize = cocos2d::Director::getInstance()->getWinSize();
     
     int x = MAX(player_sprite_->getPositionX(), winSize.width/2);
     int y = MAX(player_sprite_->getPositionY(), winSize.height/2);
     x = MIN(x, (level_map_->getMapSize().width * level_map_->getTileSize().width) - winSize.width / 2);
     y = MIN(y, (level_map_->getMapSize().height * level_map_->getTileSize().height) - winSize.height/2);
-    cocos2d::Vec2 actualPosition(x, y);
     
-    cocos2d::Vec2 centerOfView(winSize.width/2, winSize.height/2);
-    cocos2d::Vec2 viewPoint = centerOfView - actualPosition;
-    level_map_->setPosition(viewPoint);
+    cocos2d::Vec2 actual_position(x, y);
+    cocos2d::Vec2 centre_of_view(winSize.width/2, winSize.height/2);
+    
+    cocos2d::Vec2 viewport = centre_of_view - actual_position;
+    setPosition(viewport);
+    
     
 }
-
